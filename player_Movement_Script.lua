d08553edabe1cce4bc43bc245dd48ddc61fd465a
Jinxed Nation William Halling <60004450+JinxedNation@users.noreply.github.com>
2021-04-15 16:31:54 +0800

    Add files via upload

---
+++ player_Movement_Script.lua
@@ -0,0 +1,17 @@
+UseLuaInputs = false
+player_Keyboard_Input = 
+{
+    camera_Rotation_Speed = 100.0
+    player_Movement_Speed =   5.0
+
+	player_Move_Forward  = "W",
+	player_Move_Backward = "S",
+    player_Move_Left     = "A"
+    player_Move_Right    = "D" 
+
+
+    wire_Frame_Active    = "K"
+    helpful_instructions = "M"
+    quit_Game            = "X"
+
+}
